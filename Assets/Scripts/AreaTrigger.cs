3045f5496c669a55bfea180b75563ebe95aa3644
Teulis <tziegler30@gatech.edu>
2023-11-12 14:28:17 -0500

    AreaTriggers have Audio Clips now

--- Assets/Scripts/AreaTrigger.cs
+++ Assets/Scripts/AreaTrigger.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class AreaTrigger : MonoBehaviour
 {
     [SerializeField] private int AreaNumber;
+    [SerializeField] private AudioClip Music;
     [SerializeField] private Vector3 position;
     [SerializeField] private Quaternion rotation;
 
     public void EnterCheckpoint()
     {
-        //PlayMusic(AreaNumber);
+        //PlayMusic(Music);
         FindObjectOfType<PlayerSave>().SetArea(position, rotation);
     }
 
