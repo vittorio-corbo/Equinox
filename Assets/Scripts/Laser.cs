40624b40f05d9389a5345c671ce30d34ff30010e
Willzhang13 <yizhang@gatech.edu>
2023-11-15 11:18:47 -0500

    New Laser Icon 1

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.NetworkInformation;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Laser : MonoBehaviour
 {
