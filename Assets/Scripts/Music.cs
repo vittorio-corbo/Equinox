d9670be35eb7e9ee452a9062a8046724dbedd771
jvogt23 <91633361+jvogt23@users.noreply.github.com>
2023-11-27 01:30:15 -0500

    Added music fade in and out
    Not currently used, but music now fades in and out using a mixer

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -1,17 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using System;
 
 public class Music : MonoBehaviour
 {
     /**
-     * This script will be applied to the Music Generator GameObject.
-     * The purpose of the script is to read the current scene name,
-     * set the music accordingly, and begin playing the music on a loop.
-     * This allows us to have a prefab of the music generator while having 
-     * different music tracks depending on the scene.
+     * This script will be used to ensure that the correct music track is playing for a given situation
      * 
      * This script was originally created by James Vogt
      * Other Contributors:
@@ -26,28 +23,36 @@ public class Music : MonoBehaviour
     //Setting in the Update() method pls.
     public float volume;
     public float doppler;
+    public AudioMixer mix;
     private AudioSource audio;
     public bool BeatzAreDroppin = true;
+    private bool stopped = false;
 
     private bool paused;
     public AudioClip clipToUse;
 
+    private string exparam = "masterVol";
+
     // Start is called before the first frame update
     void Start()
     {
+        audio = GetComponent<AudioSource>();
         if (BeatzAreDroppin)
         {
             volume = MenuActions.musicVolume;
-            audio = gameObject.AddComponent(typeof(AudioSource)) as AudioSource;
-            sceneName = SceneManager.GetActiveScene().name;
-            BeatDrop(audio, clipToUse, volume, doppler);
+            StartMusic(clipToUse);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        audio.volume = MenuActions.musicVolume;
+        if (!BeatzAreDroppin && !stopped)
+        {
+            StopMusic();
+            stopped = true;
+        }
+        mix.SetFloat("settingsVol", Mathf.Log10(MenuActions.musicVolume) * 20);
         if (!paused && PauseScript.isPaused || paused && !(PauseScript.isPaused)) {
             pause();
         }
@@ -86,5 +91,42 @@ public class Music : MonoBehaviour
             audio.Pause();
         }
         paused = !paused;
+    }
+
+    void StartMusic(AudioClip clip)
+    {
+        audio.clip = clip;
+        audio.loop = true;
+        audio.Play();
+        StartCoroutine(StartFade(audio, mix, exparam, 3f, 1.0f, false));
+    }
+
+    //Test both this and StartMusic in a scene
+    void StopMusic()
+    {
+        StartCoroutine(StartFade(audio, mix, exparam, 3f, 0f, true));
+    }
+
+
+    public static IEnumerator StartFade(AudioSource src, AudioMixer audioMixer, string exposedParam, float duration, float targetVolume, bool stopping)
+    {
+        float currentTime = 0;
+        float currentVol;
+        audioMixer.GetFloat(exposedParam, out currentVol);
+        currentVol = Mathf.Pow(10, currentVol / 20);
+        float targetValue = Mathf.Clamp(targetVolume, 0.0001f, 1);
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            float newVol = Mathf.Lerp(currentVol, targetValue, currentTime / duration);
+            audioMixer.SetFloat(exposedParam, Mathf.Log10(newVol) * 20);
+            yield return null;
+        }
+
+        if (stopping)
+        {
+            src.Stop();
+        }
+        yield break;
     }
 }
