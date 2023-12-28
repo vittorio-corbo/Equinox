3d2e9b30ed0625b97dc413781edaf02f81e914c8
Vittorio Corbo <vittocorbo@gmail.com>
2023-12-28 03:29:02 -0300

    new action system works pretty well

--- Assets/Scripts/ActionLightCables.cs
+++ Assets/Scripts/ActionLightCables.cs
@@ -7,12 +7,28 @@ using TMPro;
 //Make this abstract
 public class ActionLightCables : Action
 {
+    //[SerializeField] public Reportee reportee;
+
     [SerializeField] public LightCables lightcable;
+    
     public override void Triggered()
     {
-        lightcable.FlipSwitch(true);
+        //if other dude is fixed : allFixed == true;
+        //if (reportee.allFixed){
+            lightcable.FlipSwitch(true);
+        //}
         //throw new System.NotImplementedException();
     }
+
+    //FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUCK
+    //THIS DOES NOT WORK IF IT ISN'T TRIGGERED
+    //wait. maybe this works if i have a pyramid to this
+    //     x
+    //    / \
+    //   x
+    // /  \
+    //reporter reportee system is lightweight
+    //what i should do is create an object that handles all of this and will have a chain of reporters and reportees
 
     //Do other shit
 }
