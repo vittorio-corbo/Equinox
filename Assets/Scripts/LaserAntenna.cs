34138b7d3e095206f7c96dc15faef243bbc19520
Vittorio Corbo <vittocorbo@gmail.com>
2024-01-18 11:07:54 -0500

    working on lazer

--- Assets/Scripts/LaserAntenna.cs
+++ Assets/Scripts/LaserAntenna.cs
@@ -9,17 +9,22 @@ public class LaserAntenna : Reportee
     GameObject lastGameObject;
     float timer;
     Coroutine currentCoroutine;
+
+    [SerializeField] GameObject lazer;
     private void Update()
     {
         if (allFixed && button.buttonDown)
         {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.forward, out hit, LayerMask.GetMask("Pizza")))
+            lazer.SetActive(true);
+            //try to get real lazer forward
+            RaycastHit hit2;
+            if (Physics.Raycast(lazer.gameObject.transform.position, lazer.gameObject.transform.forward, out hit2, LayerMask.GetMask("Pizza")))
+            //if (Physics.Raycast(lazer.gameObject.transform.position, lazer.gameObject.transform.forward, out hit2))
             {
                 GameObject go = null;
-                if (hit.transform.gameObject.GetComponent<Rigidbody>() != null)
+                if (hit2.transform.gameObject.GetComponent<Rigidbody>() != null)
                 {
-                    go = hit.transform.gameObject;
+                    go = hit2.transform.gameObject;
                 }
                 currentGameObject = go;
                 if (currentCoroutine == null)
@@ -27,7 +32,32 @@ public class LaserAntenna : Reportee
                     lastGameObject = go;
                     StartCoroutine(GameObjectTimer());
                 }
+                Debug.DrawLine(lazer.gameObject.transform.position, hit2.point, Color.green);
+                //Debug.DrawLine(transform.position, hit2.point, Color.green);
             }
+
+
+
+            //old stuff
+            // RaycastHit hit;
+            // if (Physics.Raycast(transform.position, transform.forward, out hit, LayerMask.GetMask("Pizza")))
+            // {
+                
+            //     Debug.DrawLine(transform.position, hit.point, Color.green);
+            //     GameObject go = null;
+            //     if (hit.transform.gameObject.GetComponent<Rigidbody>() != null)
+            //     {
+            //         go = hit.transform.gameObject;
+            //     }
+            //     currentGameObject = go;
+            //     if (currentCoroutine == null)
+            //     {
+            //         lastGameObject = go;
+            //         StartCoroutine(GameObjectTimer());
+            //     }
+            // }
+        }else{
+            lazer.SetActive(false);
         }
     }
     private IEnumerator GameObjectTimer()
